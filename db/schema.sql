b3cf8ba0a8e0ad21eb3bc2a73f481e725bed358b
bilalkemertas <bilalkemertas@gmail.com>
2026-05-06 11:40:46 +0300

    Update schema.sql

--- db/schema.sql
+++ db/schema.sql
@@ -1,1 +1,23 @@
+CREATE TABLE IF NOT EXISTS stok (
+    kod TEXT,
+    isim TEXT,
+    adres TEXT,
+    miktar REAL,
+    durum TEXT
+);
 
+CREATE TABLE IF NOT EXISTS hareketler (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    tarih TEXT,
+    islem TEXT,
+    kod TEXT,
+    isim TEXT,
+    adres TEXT,
+    miktar REAL,
+    user TEXT
+);
+
+CREATE TABLE IF NOT EXISTS users (
+    username TEXT PRIMARY KEY,
+    password TEXT
+);
